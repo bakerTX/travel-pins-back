512fd0f2e159a07ebba6bc3ead3940f2e647f428
Aaron Baker <aaronbaker9210@gmail.com>
2016-10-21 11:20:33 -0500

    first GET route

---
+++ routes/pins.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+var Pin = require('../models/pin');
+
+
+router.get('/', function(req,res,next){
+  Pin.find({}, function(err, pins){
+    if (err){
+      res.status(500).send();
+    } else {
+      res.json(pins);
+    }
+  })
+})
