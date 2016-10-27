b4125d7da78f0e58e83084c729ff601ba3e0dd32
Clint Grounds <clintgrounds@gmail.com>
2016-10-27 12:27:52 -0500

    cleared pins when signed in

--- models/pin.js
+++ models/pin.js
@@ -5,16 +5,14 @@ const pinSchema = {
   date: String,
   location: String,
   journal: String,
-<<<<<<< HEAD
   image: String,
   rating: Number,
   recommendation: String,
   eventAttended: String,
-  transportation: String
-=======
+  transportation: String,
   lat: Number,
   lon: Number
->>>>>>> 8d156526fe2fc218a5f0b1bd2f05d1fb737bd21e
+
 }
 
 var Pin = mongoose.model('Pin', pinSchema);
